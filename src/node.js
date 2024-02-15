aefa1fb7b5a107c6728a30405f1ae4ea36fa2953
Devon Powell <devon.f.powell@gmail.com>
2024-02-15 09:53:21 -0500

    fix: bump ecma version

--- src/node.js
+++ src/node.js
@@ -1,5 +1,5 @@
 module.exports = {
   extends: './common.js',
   env: { node: true, es6: true },
-  parserOptions: { sourceType: 'module', ecmaVersion: 2020 }
+  parserOptions: { sourceType: 'module', ecmaVersion: 2023 }
 };
